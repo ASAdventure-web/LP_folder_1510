6e41ebd874f22795b91486724a05dafbbc7fcf72
Bert van Depoele <bert.vandepoele@asadventure.com>
2015-09-25 13:43:47 +0200

    HEAD ERAF

--- folder/application/views/includes/links.php
+++ folder/application/views/includes/links.php
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 <link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/stylesheets/css/folder-juni.css?v=2">
-=======
+
 <link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/stylesheets/css/folder-juni.css?v=2">
->>>>>>> release/Aanpassen_CSS
+
  
 <!--<link rel="stylesheet" href="../../../stylesheets/css/folder-juni.css"> -->
-=======
+
 <!-- <link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/2015_zomerfolder/stylesheets/css/folder-oktober.css?v=2">
-=======
+
 <!-- <link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/stylesheets/css/folder-oktober.css?v=2">
->>>>>>> develop
+
  --> 
 <link rel="stylesheet" href="../../../stylesheets/css/folder-oktober.css"> 
->>>>>>> develop
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.bg.min.js"></script>
