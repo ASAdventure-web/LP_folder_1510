10221959105519cf8be6366ffc5b4b2200ad420e
Maaike Ongena <maaike.ongena@asadventure.com>
2015-09-28 14:34:40 +0200

    links for local view

--- folder/application/views/includes/links.php
+++ folder/application/views/includes/links.php
@@ -1,8 +1,8 @@
-<link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/stylesheets/css/folder-oktober.css?v=2">
-
-<!--  
+<!-- <link rel="stylesheet" href="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/stylesheets/css/folder-oktober.css?v=2">
+ -->
+ 
 <link rel="stylesheet" href="../../../stylesheets/css/folder-oktober.css"> 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.bg.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/javascript/filtering.min.js?v=3"></script>
