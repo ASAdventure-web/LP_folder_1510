89e1fd748c3e69f17669e7b99cd7a7e0b54fcf69
Maaike Ongena <maaike.ongena@asadventure.com>
2015-09-29 15:25:18 +0200

    javascript links aanroepen

--- folder/application/views/includes/links.php
+++ folder/application/views/includes/links.php
@@ -5,7 +5,7 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.min.js"></script>
 <script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/bower_components/lazyloadxt/dist/jquery.lazyloadxt.bg.min.js"></script>
-<script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/javascript/filtering.min.js?v=3"></script>
+<script src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/javascript/filtering-min.js?v=4"></script>
 
-<!--  <script src="../javascript/filtering.js"></script>
+<!--  <script src="../../../javascript/filtering.js"></script>
  -->
