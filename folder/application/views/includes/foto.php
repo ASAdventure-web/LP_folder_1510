05cb3dcc8850622a179807d83467d5dabea67c6f
Bert van Depoele <bert.vandepoele@asadventure.com>
2015-09-30 10:15:08 +0200

    CSS aanpassing + logo + navigatie links

--- folder/application/views/includes/foto.php
+++ folder/application/views/includes/foto.php
@@ -1,10 +1,10 @@
 <?php if (($gridcount % 3 == 0) && ($count % 4 == 0) && ($photocount % 2 == 0)) { ?>
 	<div class="col-6 row-2 image-container">
-			<img data-src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld'] ?>">
+			<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld'] ?>">
 	</div>
 <?php } ?>
 <?php if (($gridcount % 3 == 0) && ($count % 4 == 2) && ($photocount % 2 == 1)) { ?>
 	<div class="col-6 row-2 image-container right">
-			<img data-src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld'] ?>">
+			<img class="image" data-src="http://static.asadventure.com/includes/landingpage/2015_herfstfolder/images/beelden/nl/<?php echo $photos[$photocount]['beeld'] ?>">
 	</div>
 <?php } ?>
